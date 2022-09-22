23f849ac7574789238d824cbd2b77cf509b51a33
İbrahim Geyik <geyikk.ibrahim@gmail.com>
2022-09-22 15:08:05 +0300

    get deneme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pokemonapi/pages/homepage.dart';
+//get deneme eklendi
 
 void main() {
   runApp(const MyApp());
